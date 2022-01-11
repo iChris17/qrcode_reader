16d9f76a24da6a180a06cb18c3e0ada55d796d22
Christopher Acevedo <acevedocris17@gmail.com>
2022-01-11 12:41:56 -0600

    Creating QR List Screen and styles

---
+++ src/components/SearchInput.js
@@ -0,0 +1,65 @@
+import React, { useState } from "react";
+import { View, TextInput, StyleSheet } from "react-native";
+import Icon from "react-native-vector-icons/FontAwesome5";
+
+const SearchInput = ({ onSearchEnter }) => {
+  const [term, setTerm] = useState("");
+
+  return (
+    <View style={styles.searchWrapperStyle}>
+      <Icon size={18} name="search" color="grey" style={styles.iconStyleLeft} />
+      <TextInput
+        placeholder="Type to search"
+        placeholderTextColor="grey"
+        style={styles.searchInputStyle}
+        value={term}
+        onChangeText={(newText) => {
+          setTerm(newText);
+        }}
+        onEndEditing={() => {
+          onSearchEnter(term);
+        }}
+      />
+      <Icon
+        size={18}
+        name="times"
+        color="grey"
+        style={styles.iconStyleRight}
+        onPress={() => {
+          setTerm("");
+          onSearchEnter("");
+        }}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  searchWrapperStyle: {
+    backgroundColor: "white",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    borderRadius:15,
+    margin:10
+  },
+  iconStyleLeft: {
+    marginTop: 12,
+    marginHorizontal: 8,
+    marginLeft:12
+  },
+  iconStyleRight: {
+    marginTop: 12,
+    marginHorizontal: 8,
+    marginRight:15
+  },
+  searchInputStyle: {
+    flex: 1,
+    fontSize: 16,
+    paddingVertical: 8,
+    paddingHorizontal: 0,
+    margin: 0,
+    color: "white",
+  },
+});
+
+export default SearchInput;
--- src/screens/QrList.js
+++ src/screens/QrList.js
@@ -1,12 +1,69 @@
-import React from "react";
-import { Text, SafeAreaView } from "react-native";
+import React, { useState } from "react";
+import { Text, StyleSheet, FlatList, View, SafeAreaView } from "react-native";
+import SearchInput from "../components/SearchInput";
+
+const Item = ({ item }) => {
+  return (
+    <View style={styles.containerItemList}>
+      <Text style={styles.itemText}>{item}</Text>
+    </View>
+  );
+};
 
 const QrList = () => {
+  const [QRData, setQRData] = useState([]);
+
+  const handleFilter = (valueSearch = "") => {
+    const filterList = QRData.filter((qr) => {
+      return qr.toLowerCase().includes(valueSearch.toLowerCase());
+    });
+    setQRData(filterList);
+  };
+
+  if (QRData.length === 0) {
+    return (
+      <SafeAreaView style={{ padding: 10, alignItems: "center" }}>
+        <Text style={{ fontSize: 16, fontWeight: "bold" }}>
+          There are no QR Codes scanned. Please, go and scan some
+        </Text>
+      </SafeAreaView>
+    );
+  }
+
   return (
     <SafeAreaView>
-      <Text>Qr Codes List View</Text>
+      <SearchInput onSearchEnter={handleFilter} />
+      <FlatList
+        data={QRData}
+        numColumns={1}
+        showsVerticalScrollIndicator={false}
+        keyExtractor={(data, index) => index}
+        renderItem={({ item }) => <Item item={item} />}
+        contentContainerStyle={styles.flatListcontainer}
+      />
     </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  flatListcontainer: {
+    margin: 10,
+    padding: 10,
+    marginTop: Platform.OS === "android" ? 3 : 0,
+    backgroundColor: "white",
+    borderRadius: 5,
+  },
+  containerItemList: {
+    backgroundColor: "grey",
+    marginBottom: 2,
+    alignItems: "center",
+    padding: 10,
+    borderRadius: 5,
+  },
+  itemText: {
+    fontSize: 20,
+    color: "#fff",
+  },
+});
 
 export default QrList;
